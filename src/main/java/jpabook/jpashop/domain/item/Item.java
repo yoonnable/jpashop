ac5cd050046b9390987b66c45c24687bc3a615d9
yoonnable <yoonnable@gmail.com>
2024-01-09 12:28:50 +0900

    feat: 상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //== 비즈니스 로직 ==//
+    // 데이터를 갖고있는 객체에 비즈니스 로직을 넣는 것이 객체지향적으로 좋은 코드다.
+    /**
+    * 재고 수량 증가
+    */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    /**
+    * 재고 수량 감소
+    */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
